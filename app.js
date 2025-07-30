b291f6dfd76f9852f06ba03ca05525cf2e127d4e
borys solorzano <borys.solo19@gmail.com>
2025-07-29 20:55:37 -0400

    pokedex complete

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ import { fileURLToPath } from "url";
 const app = express();
 const port = 3000;
 
-// Needed to get __dirname in ES modules
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve static files from the "public" folder
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
